d23b168f6b9b747f6a5b16a16e3ab390cb85373d
TPCT <eslamjackie@outlook.com>
2018-07-01 01:33:52 -0400

    Update __init__.py

--- python shorten url/__init__.py
+++ python shorten url/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request, make_response, session, url_for, escape, flash, abort
 from wtforms import SubmitField, PasswordField, StringField
 from wtforms.validators import DataRequired, email, url
+from werkzeug.utils import secure_filename
 from flask_wtf import FlaskForm
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -11,7 +12,7 @@ import warnings, smtplib, threading, os, time, random, string, datetime
 non_real_useragents = ['facebookexternalhit', 'Facebot', 'googlebot', 'adsbot', 'bingbot', 'Slurp']
 warnings.filterwarnings('ignore')
 main_urls = ('index', 'login', 'signup')
-dash_board_urls = ('dashboardindex', 'dashboard')
+dashboard_urls = ('dashboardindex', 'dashboard')
 user_info = namedtuple('user_info', ['username', 'email', 'password', 'phone_number'])
 new_user_info = namedtuple('new_user_info',
 						   ['usersname', 'email', 'password', 'phone_number', 'link', 'session_time'])
@@ -22,11 +23,11 @@ shorted_urls = {}
 emails_address = []
 active_links = []
 settings = {'users_table': os.path.join(os.path.dirname(__file__), 'templates/database/users_table.bin'),
-			'new_users': os.path.join(os.path.dirname(__file__), 'templates/database/new_users.bin'),
-			'dashboard': os.path.join(os.path.dirname(__file__), 'templates/dashboard'),
-			'shorted_urls': os.path.join(os.path.dirname(__file__), 'templates/database/shorted_urls.bin')}
+			'new_users':   os.path.join(os.path.dirname(__file__), 'templates/database/new_users.bin'),
+			'dashboard':   os.path.join(os.path.dirname(__file__), 'templates/dashboard'),
+			'shorted_urls':os.path.join(os.path.dirname(__file__), 'templates/database/shorted_urls.bin')}
 database_user_tables_fetched = database_new_users_fetched = database_shorted_url_fetched = database_fetched = False
-scrirpt_domain_name = ''
+script_domain_name = ''
 app = Flask(__name__)
 
 
@@ -37,8 +38,8 @@ class Security:
 		main_string = ''
 		for i in range(1, len(data) + 1):
 			char = ord(data[i - 1])
-			passphrase = [random.choice(list(string.printable.replace('$', '').replace('\\', ''))) for x in
-						  range(passphrase_len)]
+			pr = string.printable.translate(string.printable.maketrans({'$': '', '\\': '', '\'': '', '"': '', '|': ''}))
+			passphrase = [random.choice(list(pr)) for x in range(passphrase_len)]
 			passphrase[int(passphrase_len / 2)] = '$'
 			passphrase = ''.join(passphrase)
 			char = char + i * key
@@ -46,18 +47,6 @@ class Security:
 			encrypted_data += passphrase.replace('$', chr(int(char)))
 		encrypted_data += ' /[k=%%%s%%p=%%%s%%]/' % (key, passphrase_len)
 		return encrypted_data.encode('utf-8')
-
-	def Add_New_User(self, username, password, email_address, phone):
-		file = open(settings['new_users'], 'a+')
-		link = ''.join(
-			[random.choice(string.ascii_letters + string.digits) for x in
-			 range(random.choice(list(range(9, 100))))])
-		file.write(str(
-			Security().encryption(
-				':'.join([username, password, email_address, phone, str(time.time() + 3600), link]),
-				12)) + '\n')
-		file.close()
-		return link
 
 	def decryption(self, data):
 		args = data.split('%]/')
@@ -144,12 +133,25 @@ class database:
 				shorted_urls_file.write(str(
 					Security().encryption('%s-:-%s-:-%s' % (url[0], url[1], int(url[2]) + 1),
 										  random.randint(7, 20))) + '\n')
+				shorted_urls[url[0]] = shorted_url_info(url[0], url[1], int(url[2])+1)
 			else:
 				shorted_urls_file.write(str(
 					Security().encryption('%s-:-%s-:-%s' % (url[0], url[1], url[2]),
 										  random.randint(7, 20))) + '\n')
 		shorted_urls_file.truncate()
 		shorted_urls_file.close()
+	
+	def Add_New_User(self, username, password, email_address, phone):
+		file = open(settings['new_users'], 'a+')
+		link = ''.join(
+			[random.choice(string.ascii_letters + string.digits) for x in
+			 range(random.choice(list(range(9, 100))))])
+		file.write(str(
+			Security().encryption(
+				':'.join([username, password, email_address, phone, str(time.time() + 3600), link]),
+				12)) + '\n')
+		file.close()
+		return link
 
 	class Create_user(threading.Thread):
 		def startx(self, username, password, email_address, phone_number):
@@ -171,7 +173,7 @@ class database:
 			new_users_data = new_users_table_file.readlines()
 			new_users_table_file.seek(0)
 			for user in new_users_data:
-				Args = Security().decryption(eval(user).decode()).split(':')
+				Args = Security().decryption(eval(user).decode('utf-8')).split(':')
 				username = Args[0]
 				password = Args[1]
 				email_address = Args[2]
@@ -188,8 +190,11 @@ class database:
 			new_users_table_file.truncate()
 			new_users_table_file.close()
 
-	class file_watcher:
-		def __init__(self):
+	class file_watcher(threading.Thread):
+		def run(self):
+			self.user_tables().daemon = True
+			self.new_users().daemon = True
+			self.shorted_urls().daemon = True
 			self.user_tables().start()
 			self.shorted_urls().start()
 			self.new_users().start()
@@ -303,8 +308,11 @@ class database:
 				username = Args[0]
 				if username != self.username:
 					new_users_file.write(i)
+				else:
+					del new_users[username]
 			new_users_file.truncate()
 			new_users_file.close()
+
 
 class website_forms:
 	class shorten_form(FlaskForm):
@@ -326,21 +334,27 @@ class website_forms:
 
 
 mail_settings = {
-	"MAIL_SERVER": 'SMTP_SERVER',
-	"MAIL_PORT": 'SMTP_PORT',
+	"MAIL_SERVER": 'host10.ameronix.com',
+	"MAIL_PORT": 465,
 	"MAIL_USE_TLS": False,
 	"MAIL_USE_SSL": True,
-	"MAIL_USERNAME": 'SMTP_USERNAME',
-	"MAIL_PASSWORD": 'SMTP_PASSWORD'
+	"MAIL_USERNAME": 'wpdawson@boarcreekwhiskey.com',
+	"MAIL_PASSWORD": 'th3professionalcod3r'
 }
 app.config.update(mail_settings)
 mail = Mail(app)
 app.secret_key = Security().encryption('ABDOANDTPCT', 12)
 
 
+def main_script_url():
+	global script_domain_name
+	script_domain_name = 'http://'+request.host
+
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+	main_script_url()
 	form = website_forms().shorten_form()
 	if str(request.method).lower() == 'post':
 		if form.validate_on_submit():
@@ -360,31 +374,31 @@ def index():
 			parsed_url = random_chunk(random.randint(0, 10), random.randint(15, 60))
 			while parsed_url in shorted_urls:
 				parsed_url = random_chunk(random.randint(0, 10), random.randint(15, 60))
+			parsed_url = script_domain_name+'/link/%s'%(parsed_url)
 			shorted_urls_file = open(settings['shorted_urls'], 'a+')
 			shorted_urls_file.write(str(
-				Security().encryption(scrirpt_domain_name + 'link/%s-:-%s-:-%s' % (parsed_url, url, 0),
+				Security().encryption('%s-:-%s-:-%s' % (parsed_url, url, 0),
 									  random.randint(7, 20))) + '\n')
+			shorted_urls[parsed_url] = shorted_url_info(parsed_url, url, 0)
 			shorted_urls_file.close()
 			database().file_watcher().shorted_urls().start()
-			return render_template('index.html', form=form, valid_url=scrirpt_domain_name + 'link/%s' % parsed_url)
+			return render_template('index.html', form=form, valid_url=parsed_url)
 
 	return render_template('index.html', form=form, valid_url=None)
 
 
-@app.route('/')
+@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def Url_Catcher(path):
-	global scrirpt_domain_name
-	scrirpt_domain_name = request.host_url
+	main_script_url()
 	path = path.split('.')[0]
 	if path.lower() in main_urls:
 		return redirect('/%s' % path)
-	elif path.replace('/', '') in dash_board_urls:
+	elif path.replace('/', '') in dashboard_urls:
 		if Security().check_cookies('logged', Security().get_external_ip()):
 			return redirect('/%s' % path.strip('/'))
 		else:
-			title = 'Error - You are lost'
-			return render_template('404.html', title=title)
+			return redirect(url_for('login'))
 	else:
 		title = 'Error - You are lost'
 		return render_template('404.html', title=title)
@@ -395,8 +409,9 @@ def internal_server_error(e):
 	return render_template('404.html'), 500
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'], strict_slashes=False)
 def login():
+	main_script_url()
 	form = website_forms().Login_Form()
 	error = None
 	title = 'Login Page'
@@ -423,18 +438,19 @@ def login():
 @app.errorhandler(404)
 def page_not_found(e):
 	title = 'Error - You are lost'
+	main_script_url()
 	return render_template('404.html', title=title), 404
 
 
 @app.route('/link/<path:path>')
 def redirector(path):
-	path = scrirpt_domain_name + 'link/%s' % path
+	main_script_url()
+	path = script_domain_name + '/link/%s' % path
 	if path in shorted_urls.keys():
 		if any([x for x in non_real_useragents if x in str(request.user_agent).lower()]):
 			return ''
 		else:
-			time.sleep(30)
-			database().add_new_visitor(url)
+			database().add_new_visitor(path)
 			return redirect(shorted_urls[path][1])
 	else:
 		print(path, shorted_urls)
@@ -445,15 +461,16 @@ def redirector(path):
 def sign_up():
 	sign_up_form = website_forms().Signup_Form()
 	error = None
+	main_script_url()
 	if sign_up_form.validate_on_submit():
 		if str(request.method).lower() == 'post':
 			username = sign_up_form.username.data
 			password = sign_up_form.password.data
 			confirm_password = sign_up_form.confirm_password.data
 			email_address = sign_up_form.email_address.data
-			phone = sign_up_form.phone.data
+			phone = sign_up_form.phone.data if sign_up_form.phone.data.__len__() > 0 else ''
 			if password == confirm_password and username not in users_table.keys() and username not in new_users.keys() and email_address not in emails_address:
-				link = str(scrirpt_domain_name) + 'reg/' + Security().Add_New_User(username, password,
+				link = str(script_domain_name) + '/reg/' + database().Add_New_User(username, password,
 																					email_address, phone)
 				message = """
 				<html><head></head><body><table style="background:#2a3644;padding:40px 0 20px 0" cellspacing="0" cellpadding="0" border="0" align="center" width="800">
@@ -520,10 +537,14 @@ def dashboard_index():
 	pass
 
 
-
 if __name__ == '__main__':
-	database().file_watcher()
+	database().file_watcher().daemon = True
+	database().file_watcher().start()
 	database().database_fetched().start()
 	while not database_fetched:
 		pass
-	app.run('0.0.0.0', 81, debug=False)
+	app.run('localhost', 5000, debug=False)
+	while True:
+		if not database().file_watcher().isAlive():
+			os._exit(1)
+			break
